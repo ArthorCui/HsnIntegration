95802158c6f73ee4546dc1a178d8c8ec619e805e
Roger.Cui <jesuie@live.cn>
2014-11-13 16:33:54 +0800

    create my first wcf project for hello world

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelloWorld;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldService client = new HelloWorldService();
+            Console.ReadLine();
+            Console.WriteLine(client.Greet(new Person { Name = "Roger" }));
+            Console.WriteLine(client.GetData(1));
+        }
+    }
+}
