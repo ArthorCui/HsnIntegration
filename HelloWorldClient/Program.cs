142301673761e32518d7e9b2f6916a3f17ae4178
Roger.Cui <jesuie@live.cn>
2014-11-17 16:25:06 +0800

    I can't understand why the console application can not recognize difference configuration files

--- HelloWorldClient/Program.cs
+++ HelloWorldClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HelloWorld;
+using ServiceLibary;
 
 namespace HelloWorldClient
 {
@@ -13,6 +14,10 @@ namespace HelloWorldClient
             HelloWorldServiceClient client = new HelloWorldServiceClient();
             Console.WriteLine(client.Greet(new Person { Name = "Roger" }));
             client.Close();
+
+            ProductServiceClient client2 = new ProductServiceClient();
+            Console.WriteLine(client2.GetPrice(new Product { Name = "potato", Price = 0.55 }));
+            client2.Close();
             Console.ReadLine();
         }
     }
