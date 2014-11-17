d45fbd07503422a582f0ce3b0bbf77174587e2eb
Roger.Cui <jesuie@live.cn>
2014-11-17 14:50:07 +0800

    fix build product service by service contract

---
+++ ServiceLibary/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace ServiceLibary
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        double GetPrice(Product product);
+    }
+}
---
+++ ServiceLibary/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceLibary
+{
+    public class ProductService : IProductService
+    {
+        public double GetPrice(Product product)
+        {
+            return product.Price;
+        }
+    }
+}
