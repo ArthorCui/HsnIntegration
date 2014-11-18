d56559324933ed79dd97a8e3ebfb5ad8e8ab4206
Roger.Cui <jesuie@live.cn>
2014-11-18 14:18:21 +0800

    add windows service use tcp listener port communicate with the background service each other

---
+++ WcfTestService/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+using WcfServiceLibrary;
+
+namespace WcfTestService
+{
+    partial class Service : ServiceBase
+    {
+        internal static ServiceHost myServiceHost = null;
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+            }
+            myServiceHost = new ServiceHost(typeof(Service));
+            myServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+                myServiceHost = null;
+            }
+        }
+    }
+}
