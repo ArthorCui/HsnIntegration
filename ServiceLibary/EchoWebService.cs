b92dacde05be1bdf59e88981656c9b72a285be42
Roger.Cui <jesuie@live.cn>
2014-11-19 17:46:15 +0800

    add echo webservice to handle soap xml request

---
+++ ServiceLibary/EchoWebService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Services;
+using System.Xml;
+using System.IO;
+using System.Web;
+
+namespace ServiceLibary
+{
+    [WebService(Namespace = "localhost", Name = "ServiceLibary")]
+    public class EchoWebService : WebService
+    {
+        [WebMethod(Description = "Echo Soap Request")]
+        public XmlDocument EchoSoapRequest(int input)
+        {
+            var xmlSoapRequest = new XmlDocument();
+
+            using (Stream receiveStream = HttpContext.Current.Request.InputStream)
+            {
+                receiveStream.Position = 0;
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    xmlSoapRequest.Load(readStream);
+                }
+            }
+            return xmlSoapRequest;
+        }
+    }
+}
