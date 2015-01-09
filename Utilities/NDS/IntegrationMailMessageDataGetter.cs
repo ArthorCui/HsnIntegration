5d5568061d89924cf0b8e835ec2c2cdcc15b3ac4
Roger.Cui <jesuie@live.cn>
2015-01-09 15:03:09 +0800

    mock nds intergration mail message handle

---
+++ Utilities/NDS/IntegrationMailMessageDataGetter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.NDS
+{
+    public class IntegrationMailMessageDataGetter : IPageDataGetter
+    {
+        public virtual void Init(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Utilities/NDS/NDSLetterDetailIntegrationMailMessageDataGetter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.NDS
+{
+    public class NDSLetterDetailIntegrationMailMessageDataGetter : NDSIntegrationMailMessageDataGetter
+    {
+        public override void Init(string str)
+        {
+
+            base.Init(str);
+        }
+    }
+}
