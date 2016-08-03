ac282ef3e781a03d7c080c901cbbbec249855a45
Roger.Cui <jesuie@live.cn>
2016-08-03 17:13:10 +0800

    add ConnectionString encrypt and decrypt form application

---
+++ ConnectionString/CSForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ConnectionString
+{
+    partial class CSForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AllowDrop = true;
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(708, 352);
+            this.splitContainer1.SplitterDistance = 292;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox_input);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.textBox_output);
+            this.splitContainer2.Size = new System.Drawing.Size(708, 292);
+            this.splitContainer2.SplitterDistance = 181;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_input.Location = new System.Drawing.Point(0, 0);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_input.Size = new System.Drawing.Size(708, 181);
+            this.textBox_input.TabIndex = 0;
+            this.textBox_input.Text = "please input your encrypt/decrypt connection strings...";
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_output.Location = new System.Drawing.Point(0, 0);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_output.Size = new System.Drawing.Size(708, 107);
+            this.textBox_output.TabIndex = 0;
+            this.textBox_output.Text = "output result will be here";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CSForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 352);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "CSForm";
+            this.Text = "CSForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_output;
+    }
+}
+
---
+++ ConnectionString/CSForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Utilities.Entriq;
+
+namespace ConnectionString
+{
+    public partial class CSForm : Form
+    {
+        public string message = string.Empty;
+
+        public CSForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox_output.Text = string.Empty;
+
+            var connectionStrings =GetConnectionStrings(this.textBox_input.Text);
+
+            var result = EncryptOrDecrypt(connectionStrings);
+
+            result.Values.ToList().ForEach(x =>
+            {
+                this.textBox_output.Text += string.Format("{0} \r\n", x);
+            });
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show(message);
+            }
+        }
+
+        private List<string> GetConnectionStrings(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+            {
+                MessageBox.Show("Please input Encrypt/Decrypt connection strings...");
+            }
+            var array = inputString.Split(new string[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+            return array.ToList();
+        }
+
+        private Dictionary<string, string> EncryptOrDecrypt(List<string> connectionStrings)
+        {
+            var outputDic = new Dictionary<string, string>();
+            Encryption instance = new Encryption();
+
+            if (connectionStrings.Count > 0)
+            {
+                connectionStrings.ForEach(x =>
+                {
+                    if (x.StartsWith("v3basic"))
+                    {
+                        if (instance.Decrypt(x))
+                        {
+                            outputDic.Add(x, instance.Result);
+                        }
+                    }
+                    else
+                    {
+                        if (instance.Encrypt(x))
+                        {
+                            //if key is exsit
+                            if (outputDic.ContainsKey(x))
+                            {
+                                message += string.Format("input string contains duplicate item:\r\n{0}", x);
+                            }
+                            outputDic.Add(x, instance.Result);
+                        }
+                    }
+                });
+            }
+            return outputDic;
+        }
+    }
+}
