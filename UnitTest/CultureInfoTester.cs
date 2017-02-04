f530710e26c0c5a84669f0039d02706893cc4fc0
Roger.Cui <jesuie@live.cn>
2017-02-04 11:30:37 +0800

    add cultureinfo test

---
+++ UnitTest/CultureInfoTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class CultureInfoTester
+    {
+        [Test]
+        public void currency_by_server_test()
+        {
+            foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                Console.WriteLine(string.Format("Name: {0}, English Name: {1}\n", culture.Name, culture.EnglishName));
+            }
+        }
+
+        [Test]
+        public void currency_india_test()
+        {
+            var culture = CultureInfo.GetCultureInfo("en-IN");
+
+            Assert.AreEqual(".", culture.NumberFormat.CurrencyDecimalSeparator);
+            Assert.AreEqual(",", culture.NumberFormat.CurrencyGroupSeparator);
+            Assert.AreEqual(new int[] { 3, 2 }, culture.NumberFormat.CurrencyGroupSizes);
+
+        }
+    }
+}
