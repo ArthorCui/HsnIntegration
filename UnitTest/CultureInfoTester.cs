7669a2f988d393d965509bbfe12787cf5cbe42c4
Roger.Cui <jesuie@live.cn>
2017-02-04 14:17:08 +0800

    add some currency regex match tests

--- UnitTest/CultureInfoTester.cs
+++ UnitTest/CultureInfoTester.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace UnitTest
@@ -28,6 +29,31 @@ namespace UnitTest
             Assert.AreEqual(",", culture.NumberFormat.CurrencyGroupSeparator);
             Assert.AreEqual(new int[] { 3, 2 }, culture.NumberFormat.CurrencyGroupSizes);
 
+        }
+
+        [Test]
+        public void currency_regex_match_test()
+        {
+            var standard_pattern = "^\\s*[+-]?(\\d{1,3}(,\\d{3})*(\\.\\d+)?|\\d+(\\.\\d+)?)\\s*$";
+
+            Assert.AreEqual(true, VerifyMatch("-12,122,100.00", standard_pattern));
+            Assert.AreEqual(true, VerifyMatch("-12,122,100.00", standard_pattern));
+            Assert.AreEqual(false, VerifyMatch("-121,221,00.00", standard_pattern));
+            Assert.AreEqual(false, VerifyMatch("-1,21,22,100.00", standard_pattern));
+
+            var india_pattern = "^\\s*[+-]?(\\d{1,3}((,\\d{2})*(,\\d{3})?)(\\.\\d+)?|\\d+(\\.\\d+)?)\\s*$";
+            Assert.AreEqual(true, VerifyMatch("-1,21,22,100.00", india_pattern));
+            Assert.AreEqual(true, VerifyMatch("-100.00", india_pattern));
+            Assert.AreEqual(true, VerifyMatch("-1,100.00", india_pattern));
+            Assert.AreEqual(false, VerifyMatch("-1231,100.00", india_pattern));
+
+        }
+
+        private bool VerifyMatch(string inputString, string pattern)
+        {
+            var regex = new Regex(pattern);
+            Match match = regex.Match(inputString);
+            return match.Success;
         }
     }
 }
